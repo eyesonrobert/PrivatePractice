bcdfa72e64e711488dfd53253cad5ad498ff23fc
Robert Franklin <eyesonrobert@gmail.com>
2018-12-02 07:31:44 -0800

    routes and scaffolding applied

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,43 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import { BrowserModule } from "@angular/platform-browser";
+import { NgModule } from "@angular/core";
 
-import { AppComponent } from './app.component';
+import { AppComponent } from "./app.component";
+import { HeaderComponent } from "./header/header.component";
+import { AboutmeComponent } from "./aboutme/aboutme.component";
+import { ContactmeComponent } from "./contactme/contactme.component";
+import { Routes, RouterModule } from "@angular/router";
+import { HomeComponent } from "./home/home.component";
+import { HomeCarouselComponent } from "./home/home-carousel/home-carousel.component";
+import { SpecialtiesComponent } from "./specialties/specialties.component";
+import { ResourcesComponent } from "./resources/resources.component";
+import { FAQComponent } from "./faq/faq.component";
+import { BlogComponent } from "./blog/blog.component";
+
+const appRoutes: Routes = [
+  { path: "aboutMe", component: AboutmeComponent },
+  { path: "contactMe", component: ContactmeComponent },
+  { path: "home", component: HomeComponent },
+  { path: "specialties", component: SpecialtiesComponent },
+  { path: "resources", component: ResourcesComponent },
+  { path: "faq", component: FAQComponent },
+  { path: "blog", component: BlogComponent }
+];
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    AboutmeComponent,
+    ContactmeComponent,
+    HomeComponent,
+    HomeCarouselComponent,
+    SpecialtiesComponent,
+    ResourcesComponent,
+    FAQComponent,
+    BlogComponent
   ],
-  imports: [
-    BrowserModule
-  ],
+  imports: [BrowserModule, RouterModule.forRoot(appRoutes)],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {}
